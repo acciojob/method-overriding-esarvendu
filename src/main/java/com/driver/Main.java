24d79fb813a126904960cdbb76b54c83f87d9838
Sarvendu   kr Pandey <88503595+esarvendu@users.noreply.github.com>
2023-04-02 18:42:56 +0530

    methode overriding solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,29 +1,27 @@
 package com.driver;
 
-class A {
-    public String meth() {
-
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A {
-    @Override
-    public String meth() {
-
-        return "Method is overridden in extended class B";
-    }
-}
-
 public class Main {
-    public static void main(String[] args) {
-        A a = new B(); // create object of class B and assign it to type A
 
-        System.out.println(a.meth()); // call meth from object of class A
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
 
         B b = new B();
+        b.meth();
 
-        System.out.println(b.meth()); // call overridden meth from object of class B
+        B b1 = new B();
+        b1.meth();
     }
 }
-  
